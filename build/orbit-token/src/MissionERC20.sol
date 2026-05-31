81d725767243f54dd2a21a4009bde4f75b9a541e
Mugisha Gad <mugishagad09@gmail.com>
2026-05-31 20:08:10 +0200

    Add files via upload

--- build/orbit-token/src/MissionERC20.sol
+++ build/orbit-token/src/MissionERC20.sol
@@ -1,1 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract MissionERC20 {
+    string public constant name = "Orbit Token";
+    string public constant symbol = "ORBIT";
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    constructor() {
+        totalSupply = 1000000000000000000000000;
+        balanceOf[msg.sender] = 1000000000000000000000000;
+        emit Transfer(address(0), msg.sender, 1000000000000000000000000);
+    }
+    function transfer(address to, uint256 value) external returns (bool) {
+        require(to != address(0), "zero address");
+        require(balanceOf[msg.sender] >= value, "balance");
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    function approve(address spender, uint256 value) external returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {
+        require(to != address(0), "zero address");
+        require(balanceOf[from] >= value, "balance");
+        uint256 allowed = allowance[from][msg.sender];
+        require(allowed >= value, "allowance");
+        if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - value;
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+}
